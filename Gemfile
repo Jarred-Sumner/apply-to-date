c882157aec530c35ac845f7fc05b05d6f63708bd
Jarred Sumner <jarred@jarredsumner.com>
2018-02-13 23:26:38 -0800

    Add sentry

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem 'premailer-rails'
 gem "letter_opener", :group => :development
 # Use Capistrano for deployment
 gem 'capistrano-rails', group: :development
+gem "sentry-raven"
+
 
 gem 'unicorn'
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -23,6 +23,8 @@ set :deploy_to, "/home/rails/rails_project"
 # Default value for :linked_files is []
 append :linked_files, "config/secrets.yml.key"
 
+set :unicorn_rack_env, "production"
+
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
