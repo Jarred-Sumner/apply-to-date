02e0f63be8c810f690e50d131104c68e29cc0484
lucyguo94@gmail.com <lucyguo94@gmail.com>
2018-02-16 16:41:48 -0800

    mail code

--- app/mailers/applications_mailer.rb
+++ app/mailers/applications_mailer.rb
@@ -12,6 +12,14 @@ class ApplicationsMailer < ApplicationMailer
     mail to: @application.email, subject: "#{@profile.name} wants to go on a date with you."
   end
 
+  def pending_app(application_id)
+    @application = Application.find(application_id)
+    @profile = @application.profile
+    @profile_name = @profile.name
+
+    mail to: @profile.user.email, subject: "#{@application.name} asked you out!"
+  end
+
   def confirmed(application_id)
     @application = Application.find(application_id)
     @profile = @application.profile
