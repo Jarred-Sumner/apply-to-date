3ca05d634e68e11c6a155a5aa179e8e85dc5c63f
Jarred Sumner <jarred@jarredsumner.com>
2018-02-17 00:23:15 -0800

    Fix empty

--- dateme-frontend/components/ReviewApplicationContainer.js
+++ dateme-frontend/components/ReviewApplicationContainer.js
@@ -6,39 +6,67 @@ import Text from "./Text";
 import ApplicationsBreadcrumbs from "./ApplicationsBreadcrumbs";
 import InlineTextForm from "./InlineTextForm";
 import copy from "copy-to-clipboard";
+import SharableSocialLink from "./SharableSocialLink";
 
-const EmptyState = ({ profileId }) => (
-  <div className="Container">
-    <div className="Title">
-      <Text align="center" type="title">
-        You have no new applications.
-      </Text>
+const EmptyState = ({ profileId }) => {
+  const url = `${process.env.DOMAIN}/${profileId}`;
+  return (
+    <div className="Container">
+      <div className="Title">
+        <Text align="center" type="title">
+          You have no new applications.
+        </Text>
+      </div>
+      <div className="Share">
+        <Text align="center" type="subtitle">
+          Share your page to meet interesting people.
+        </Text>
+      </div>
+
+      <InlineTextForm
+        type="url"
+        value={url}
+        readOnly
+        onSubmit={() => copy(url)}
+        buttonChildren={"Copy URL"}
+      />
+
+      <div>
+        <SharableSocialLink
+          provider="twitter"
+          width="48px"
+          height="48px"
+          url={url}
+        />
+
+        <SharableSocialLink
+          provider="facebook"
+          width="48px"
+          height="48px"
+          url={url}
+        />
+      </div>
+      <style jsx>{`
+        .Container {
+          display: grid;
+          grid-template-rows: 1fr 1fr 1fr 1fr;
+          grid-row-gap: 28px;
+          justify-content: center;
+          text-align: center;
+        }
+
+        div {
+          display: grid;
+          margin-left: auto;
+          grid-auto-flow: column;
+          grid-column-gap: 28px;
+          justify-content: space-around;
+          margin-right: auto;
+        }
+      `}</style>
     </div>
-    <div className="Share">
-      <Text align="center" type="subtitle">
-        Share your page to meet interesting people.
-      </Text>
-    </div>
-
-    <InlineTextForm
-      type="url"
-      value={`${process.env.DOMAIN}/${profileId}`}
-      readOnly
-      onSubmit={() => copy(`${process.env.DOMAIN}/${profileId}`)}
-      buttonChildren={"Copy URL"}
-    />
-
-    <style jsx>{`
-      .Container {
-        display: grid;
-        grid-template-rows: 1fr 1fr 1fr;
-        grid-row-gap: 14px;
-        justify-content: center;
-        text-align: center;
-      }
-    `}</style>
-  </div>
-);
+  );
+};
 
 export default ({ application, isLoading, onYes, onNo, currentUser }) => (
   <Page isLoading={isLoading}>
---
+++ dateme-frontend/components/SharableSocialLink.js
@@ -0,0 +1,43 @@
+import SocialLink from "./SocialLink";
+import invariant from "invariant";
+import {
+  FacebookShareButton,
+  GooglePlusShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+  TelegramShareButton,
+  WhatsappShareButton,
+  PinterestShareButton,
+  VKShareButton,
+  OKShareButton,
+  RedditShareButton,
+  TumblrShareButton,
+  LivejournalShareButton,
+  EmailShareButton
+} from "react-share";
+
+const PROVIDER_TO_SHARE_BUTTON = {
+  facebook: FacebookShareButton,
+  twitter: TwitterShareButton
+};
+
+export default ({ provider, url, appId, width, height, ...otherProps }) => {
+  const ShareButton = PROVIDER_TO_SHARE_BUTTON[provider];
+  invariant(ShareButton, "provider must be twitter or facebook");
+
+  return (
+    <ShareButton
+      url={url}
+      hashtag={provider === "facebook" ? "#applytodate" : undefined}
+      hashtags={provider === "twitter" ? ["applytodate"] : undefined}
+    >
+      <SocialLink
+        active={true}
+        width={width}
+        hoverable
+        height={height}
+        provider={provider}
+      />
+    </ShareButton>
+  );
+};
