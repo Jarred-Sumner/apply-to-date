8ec646cc6c7dcb3538282a1a4846682de750563e
Jarred Sumner <jarred@jarredsumner.com>
2018-02-19 19:45:20 -0800

    Blacklist usernames

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  BLACKLISTED_USERNAMES = File.readlines("#{Rails.root}/config/blacklisted-usernames.txt").map(&:strip)
   authenticates_with_sorcery!
   has_one :profile
   has_many :external_authentications, through: :profile
@@ -10,4 +11,8 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true, format: /\A([^@\s]+)@((?:[-a-z0-9l]+\.)+[a-z]{2,})\Z/i
   validates :username, uniqueness: true, presence: true, format: { with: /[a-zA-Z0-9\-\_\.]*/ }
   validates :sex, presence: true, inclusion: { in: ['male', 'female', 'other']}
+
+  def self.blacklisted_username?(username)
+    BLACKLISTED_USERNAMES.include?(username)
+  end
 end
