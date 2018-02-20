ebb6360aee5caff85c6b2d2e30f8ffaf0e4842d5
Jarred Sumner <jarred@jarredsumner.com>
2018-02-20 14:48:51 -0800

    Fix new URLs

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -10,11 +10,14 @@ class Api::V1::ImagesController < Api::V1::ApplicationController
     presigner = Aws::S3::Presigner.new
     url = presigner.presigned_url(:put_object, bucket: Rails.application.secrets[:bucket_name], key: key, acl: 'public-read')
 
+    public_uri = Addressable::URI.parse(url.to_s.split('?').first)
+    public_uri.host = Rails.application.secrets[:asset_host]
+
     render json: {
       signedUrl: url.to_s,
       filename: key,
       originalFilename: params[:objectName],
-      publicUrl: url.to_s.split('?').first
+      publicUrl: public_uri.to_s
     }
   end
 
