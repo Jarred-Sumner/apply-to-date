02e82b2c0d874b007c6f1fec434e065beb0a154c
Jarred Sumner <jarred@jarredsumner.com>
2018-02-21 10:52:05 -0800

    Cloudflare with fail-over S3 too

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -11,7 +11,12 @@ class Api::V1::ImagesController < Api::V1::ApplicationController
     url = presigner.presigned_url(:put_object, bucket: Rails.application.secrets[:bucket_name], key: key, acl: 'public-read')
 
     public_uri = Addressable::URI.parse(url.to_s.split('?').first)
-    public_uri.host = Rails.application.secrets[:asset_host]
+
+    if Rails.application.secrets[:upload_host] == 'digitalocean'
+      public_uri.host = Rails.application.secrets[:asset_host]
+    else
+      public_uri.host = Rails.application.secrets[:s3_asset_host]
+    end
 
     render json: {
       signedUrl: url.to_s,
