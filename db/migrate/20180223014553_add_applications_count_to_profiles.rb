4793ace397a80321ac526de577c767e163fbe2e3
Jarred Sumner <jarred@jarredsumner.com>
2018-02-23 18:52:54 -0800

    Ooops

--- db/migrate/20180223014553_add_applications_count_to_profiles.rb
+++ db/migrate/20180223014553_add_applications_count_to_profiles.rb
@@ -3,7 +3,7 @@ class AddApplicationsCountToProfiles < ActiveRecord::Migration[5.1]
     add_column :profiles, :applications_count, :integer, default: 0, null: false
 
     Profile.reset_column_information
-    Profile.find_all do |p|
+    Profile.find_each do |p|
       Profile.reset_counters p.id, :applications
     end
   end
