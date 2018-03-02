b26140e16efb58ca4a26e6905932d3c0304a06ac
lucyguo94@gmail.com <lucyguo94@gmail.com>
2018-03-02 13:42:55 -0800

    typo

--- dateme-frontend/pages/PrivacyPolicy.js
+++ dateme-frontend/pages/PrivacyPolicy.js
@@ -176,7 +176,7 @@ class PrivacyPolicy extends React.PureComponent {
                 <p>
                   These Services do not address anyone under the age of 13. We
                   do not knowingly collect personally identifiable information
-                  from children under 13. In the case we shuffle that a child
+                  from children under 13. In the case we discover that a child
                   under 13 has provided us with personal information, we
                   immediately delete this from our servers. If you are a parent
                   or guardian and you are aware that your child has provided us
