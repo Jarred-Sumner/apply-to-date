2e74427658be351ff8e883ee662b9cdcb66e4c9e
Jarred Sumner <jarred@jarredsumner.com>
2018-03-04 11:58:47 -0800

    Add server-side error handling

--- dateme-frontend/server.js
+++ dateme-frontend/server.js
@@ -2,11 +2,17 @@ const next = require("next");
 const routes = require("./routes");
 const app = next({ dev: process.env.NODE_ENV !== "production" });
 const handler = routes.getRequestHandler(app);
+const Raven = require("raven");
+
+Raven.config(
+  "https://91be53bb2edc4c73a00df349bfc52de2:c820e9bff08d4e6e8706ca880cb67650@sentry.io/291484"
+).install();
 
 const express = require("express");
 
 app.prepare().then(() => {
   express()
+    .use(Raven.errorHandler())
     .use(handler)
     .listen(parseInt(process.env.PORT || 3000, 10));
 });
