1ffaf143099122703e1c6897951aaf4b469d0f89
Jarred Sumner <jarred@jarredsumner.com>
2018-03-15 18:45:34 -0700

    Fix missing embeds

--- app/controllers/api/v1/feeds_controller.rb
+++ app/controllers/api/v1/feeds_controller.rb
@@ -1,35 +1,14 @@
 class Api::V1::FeedsController < Api::V1::ApplicationController
+  before_action :require_external_authentication
 
   def show
-    if params[:profile_id].blank? && params[:application_id].blank?
-      return render_error(message: "Profile or application is required")
-    end
-
-    if params[:profile_id].present?
-      profile = Profile.visible.find(params[:profile_id])
-
-      if profile.social_links[params[:provider]].blank?
-        return render_error(message: "No #{params[:provider]} associated with #{params[:profile_id]} on Apply to Date")
-      end
-
-      @external_authentication = profile.external_authentications.find_by!(provider: params[:provider])
-    elsif params[:application_id].present?
-      application = current_user.received_applications.find(params[:application_id])
-
-      if application.social_links[params[:provider]].blank? && !application.external_authentications.where(provilder: params[:provider]).exists?
-        return render_error(message: "No #{params[:provider]} associated with application on Apply to Date")
-      end
-
-      @external_authentication = application.external_authentications.find_by!(provider: params[:provider])
-    end
-
     if params[:provider] == 'instagram'
       render json: {
-        data: @external_authentication.instagram.recent_posts[:data],
-        profile: @external_authentication.instagram.profile[:data]
+        data: external_authentication.instagram.recent_posts[:data],
+        profile: external_authentication.instagram.profile[:data]
       }
     elsif params[:provider] == 'twitter'
-      timeline = @external_authentication.twitter.user_timeline(@external_authentication.username)
+      timeline = external_authentication.twitter.user_timeline(@external_authentication.username)
       if tweet = timeline.try(:first)
         if tweet.user.protected?
           render_error(message: "No #{params[:provider]} associated with #{params[:profile_id] || 'application'} on Apply to Date")
@@ -51,5 +30,45 @@ class Api::V1::FeedsController < Api::V1::ApplicationController
 
   private def show_params
     params.permit(:profile_id, :provider)
+  end
+
+  private def require_external_authentication
+    if params[:profile_id].blank? && params[:application_id].blank?
+      return render_error(message: "Profile or application is required")
+    end
+
+    if external_authentication.nil?
+      render_error(message: "No #{params[:provider]} associated with #{params[:profile_id] || 'application'} on Apply to Date")
+    end
+  end
+
+  private def external_authentication
+    return @external_authentication if @external_authentication.present?
+
+    profile_id = params[:profile_id]
+
+    if params[:application_id].present?
+      application = current_user.received_applications.find(params[:application_id])
+
+      if application.social_links[params[:provider]].blank?
+        return render_error(message: "No #{params[:provider]} associated with application on Apply to Date")
+      end
+
+      profile_id ||= application.try(:applicant).try(:username)
+
+      @external_authentication = application.external_authentications.find_by(provider: params[:provider])
+    end
+
+    if profile_id.present? && !@external_authentication
+      profile = Profile.visible.find(profile_id)
+
+      if profile.social_links[params[:provider]].blank?
+        return render_error(message: "No #{params[:provider]} associated with #{profile_id} on Apply to Date")
+      end
+
+      @external_authentication = profile.external_authentications.find_by!(provider: params[:provider])
+    end
+
+    @external_authentication
   end
 end
