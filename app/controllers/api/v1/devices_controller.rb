e53575cbb6514c7c5c7a04122b06c8fadf76a7b0
Jarred Sumner <jarred@jarredsumner.com>
2018-03-26 14:57:34 -0700

    test

--- app/controllers/api/v1/devices_controller.rb
+++ app/controllers/api/v1/devices_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::DevicesController < Api::V1::ApplicationController
 
 
   def update
-    Device.update_from_headers(request.headers, current_user)
+    @current_device = Device.update_from_headers(request.headers, current_user)
 
     current_device.update(update_params.merge(
       push_enabled: params[:push_enabled].present? ? params[:push_enabled] === 'true' : current_device.push_enabled?
