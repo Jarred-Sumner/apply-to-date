9c2234fb649feef271db7a0680731029b3869ea8
Jarred Sumner <jarred@jarredsumner.com>
2018-03-31 16:53:00 -0700

    Disalbe push in dev

--- app/notifications/push_notification.rb
+++ app/notifications/push_notification.rb
@@ -2,11 +2,17 @@ class PushNotification < Struct.new(:device)
   include ApplicationHelper
 
   def send_notification!(contents: {}, headings: {}, params: {})
-    OneSignal::Notification.create(params: params.merge(
+    merged_params = params.merge(
       include_player_ids: [device.onesignal_uid],
       app_id: Rails.application.secrets[:onesignal_app_id],
       contents: contents,
       headings: headings,
-    ))
+    )
+
+    if Rails.env.development?
+      Rails.logger.info "PUSH: #{merged_params.inspect}"
+    else
+      OneSignal::Notification.create(params: merged_params)
+    end
   end
 end
